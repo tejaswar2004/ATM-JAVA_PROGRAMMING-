0f8bd6b92c54370b8245115caaf2cf7bc6fa1c93
Tejash2004 <117030163+Tejash2004@users.noreply.github.com>
2023-04-11 21:00:13 +0530

    javaproject

---
+++ src/PROJECT/ATM.java
@@ -0,0 +1,16 @@
+package PROJECT;
+
+import java.io.IOException;
+
+public class ATM {
+
+	public static void main(String[] args) throws IOException {
+		OptionMenu optionMenu = new OptionMenu();
+		introduction();
+		optionMenu.mainMenu();
+	}
+
+	public static void introduction() {
+		System.out.println("Welcome to the ATM Project!");
+	}
+}
